e38882b1477528ee2a20d32a4e48ef0236c625fd
akash <akasky70@gmail.com>
2019-11-13 13:31:37 +0545

    add show more movies button to show more movies and load default poster incase of poster load error

--- src/component/Movies.js
+++ src/component/Movies.js
@@ -1,34 +1,56 @@
-import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 import MovieCard from './MovieCard';
 
+import { withData, withError } from '../utils/common';
+
+const fetchMovieList = async (page, limit) => {
+  try {
+    let { data: res } = await axios.get(
+      `https://yts.lt/api/v2/list_movies.json?page=${page}&&limit=${limit}`
+    );
+
+    return withData(res.data.movies);
+  } catch (err) {
+    console.log(err);
+
+    return withError({
+      message: 'Opps! Something went wrong while fetching the data'
+    });
+  }
+};
+
 const Movies = () => {
+  const limit = 9;
+  const [error, setError] = useState('');
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     (async () => {
-      try {
-        let { data: res } = await axios.get('https://yts.lt/api/v2/list_movies.json')
-        setMovies(res.data.movies)
-        setIsLoading(false);
-      } catch (err) {
-        console.log(err);
-        setIsLoading(false);
-        setError('Opps! Something went wrong while fetching the data')
-      }
-    })()
-  }, []);
+      setIsLoading(true);
+      const { data, error } = await fetchMovieList(currentPage, limit);
 
-  
+      if (error) {
+        console.log(error);
+        setIsLoading(false);
+
+        return setError(error.message);
+      }
+      
+      setMovies(movies => [...movies, ...data]);
+      setIsLoading(false);
+    })();
+  }, [currentPage]);
+
   let errorMsg = error && <span>{error}</span>;
   let circularLoading = isLoading && (
     <div className="spinner-border" role="status">
       <span className="sr-only">Loading...</span>
     </div>
-  )
+  );
 
   return (
     <>
@@ -36,14 +58,24 @@ const Movies = () => {
       <div className="container">
         <div className="row justify-content-center m-5">
           {errorMsg}
-          {circularLoading}
-          {movies.map((movie) => {
-            return <MovieCard key={movie.id} movie={movie} />
+          {movies.map(movie => {
+            return <MovieCard key={movie.id} movie={movie} />;
           })}
+
+          <div className="col-md-12 m-0">{circularLoading}</div>
+
+          {!isLoading && (
+            <div
+              className="cursor-pointer movie-details-btn"
+              onClick={() => setCurrentPage(currentPage + 1)}
+            >
+              Show more movies
+            </div>
+          )}
         </div>
       </div>
     </>
   );
-}
+};
 
 export default Movies;
