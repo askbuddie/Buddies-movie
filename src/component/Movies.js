b2b11f911013aadcc49f10d116643b5c041a4ae7
Aadarsha <connectwithadarsha@gmail.com>
2020-08-01 03:51:14 +0545

    :recycle: Refactor(api) : Api call moved out from component
    Instance of axios has been created and called from component

--- src/component/Movies.js
+++ src/component/Movies.js
@@ -1,22 +1,24 @@
-import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-
+import yts from '../api';
 import MovieCard from './MovieCard';
 
 import { withData, withError } from '../utils/common';
 
 const fetchMovieList = async (page, limit) => {
   try {
-    let { data: res } = await axios.get(
-      `https://yts.mx/api/v2/list_movies.json?page=${page}&&limit=${limit}`
-    );
-
+    let { data: res } = await yts.get(`api/v2/list_movies.json/`, {
+      params: {
+        page,
+        limit,
+      },
+    });
+    console.log(res)
     return withData(res.data.movies);
   } catch (err) {
     console.log(err);
 
     return withError({
-      message: 'Opps! Something went wrong while fetching the data'
+      message: 'Opps! Something went wrong while fetching the data',
     });
   }
 };
@@ -39,34 +41,34 @@ const Movies = () => {
 
         return setError(error.message);
       }
-      
-      setMovies(movies => [...movies, ...data]);
+
+      setMovies((movies) => [...movies, ...data]);
       setIsLoading(false);
     })();
   }, [currentPage]);
 
   let errorMsg = error && <span>{error}</span>;
   let circularLoading = isLoading && (
-    <div className="spinner-border" role="status">
-      <span className="sr-only">Loading...</span>
+    <div className='spinner-border' role='status'>
+      <span className='sr-only'>Loading...</span>
     </div>
   );
 
   return (
     <>
       {console.log(movies)}
-      <div className="container">
-        <div className="row justify-content-center m-5">
+      <div className='container'>
+        <div className='row justify-content-center m-5'>
           {errorMsg}
-          {movies.map(movie => {
+          {movies.map((movie) => {
             return <MovieCard key={movie.id} movie={movie} />;
           })}
 
-          <div className="col-md-12 m-0">{circularLoading}</div>
+          <div className='col-md-12 m-0'>{circularLoading}</div>
 
           {!isLoading && (
             <div
-              className="cursor-pointer movie-details-btn"
+              className='cursor-pointer movie-details-btn'
               onClick={() => setCurrentPage(currentPage + 1)}
             >
               Show more movies
